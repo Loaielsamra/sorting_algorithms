39def7bfe16a9274e54da42f42294bc680522118
Loaielsamra <loaielsamra@gmail.com>
2023-10-21 09:41:40 -0700

    0th 2nd

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,19 +9,25 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp;
+	int tmp, swapped;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = i; j < size; j++)
+		swapped = 0;
+
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[j])
+			if (array[j] > array[j + 1])
 			{
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				swapped = 1;
 				print_array(array, size);
 			}
 		}
+
+		if (swapped == 0)
+			break;
 	}
 }
